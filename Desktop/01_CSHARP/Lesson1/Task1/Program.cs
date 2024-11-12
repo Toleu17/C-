7f4be078cf348583aace1142c30bfc48fa57972b
Toleu Aigerim <sunny.1922@inbox.ru>
2024-11-12 10:20:43 +0500

    added some variables lessom1:Task1

--- Desktop/01_CSHARP/Lesson1/Task1/Program.cs
+++ Desktop/01_CSHARP/Lesson1/Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//System.Console.WriteLine("Hello World");
+
+
+int value = 100;
+System.Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
