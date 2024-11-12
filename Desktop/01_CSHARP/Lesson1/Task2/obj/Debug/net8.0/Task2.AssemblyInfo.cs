27f095fd7209f3bf72ed21339d75392ec3fb3722
Toleu Aigerim <sunny.1922@inbox.ru>
2024-11-12 10:36:00 +0500

    add lesson1:Task2

--- Desktop/01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Desktop/01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4be078cf348583aace1142c30bfc48fa57972b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038ad1fd1b25608a9797358513d07b71070c43df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
